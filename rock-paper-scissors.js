8363388c872c09e788a13907d47f2101f1c77794
Suraj Vinti <surajvinti10@gmail.com>
2024-08-13 09:23:54 -0400

    Create getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,19 @@
 "use strict";
+// Create function to get the computer's choice
+// Randomly returns rock, paper or scissors.
+function getComputerChoice() {
+    let computerChoice;
+    computerChoice = Math.floor(Math.random() * 3) + 1;
+    
+    if (computerChoice === 1) {
+        return "rock";
+    }
+    else if (computerChoice === 2) {
+        return "paper";
+    }
+    else if (computerChoice === 3) {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
